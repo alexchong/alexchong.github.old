a50edf618bd3a5e6097709e27aefe8adcef05e03
Alex Chong <alexbchong@pm.me>
2021-01-26 13:02:57 -0800

    Update Tailwind config w/ base site colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  // darkMode: false, // or 'media' or 'class'
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pidgin: {
+          light: "#4660F6",
+          dark: "#232739",
+        },
+        stickyNote: "#f9f4ec",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
