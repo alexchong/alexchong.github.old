b83101036531f37ae02208ed6e48c956d4dba485
Alex Chong <alexbchong@pm.me>
2021-01-26 22:06:23 -0800

    Exclude current basePath in NextJS config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const user = process.env.USER;
 const isLocalDev = process.env.NVM_DIR === "/home/" + user + "/.nvm";
 
 module.exports = {
-  basePath: isLocalDev ? "" : "/alexchong.github.io",
+  //basePath: isLocalDev ? "" : "/alexchong.github.io",
   assetPrefix: isLocalDev ? "." : "/alexchong.github.io/",
 };
