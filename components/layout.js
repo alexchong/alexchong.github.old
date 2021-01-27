2d6e7e303ce01001cb282ad2610c93f7aefdb694
Alex Chong <alexbchong@pm.me>
2021-01-27 00:02:44 -0800

    Update Layout to be more cozy across mobile/desktop

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Header from "./header";
 
 export default function Layout({ children }) {
   return (
-    <main className="container mx-auto max-w-2xl px-5 md:px-8">
+    <main className="container mx-auto max-w-2xl px-5 md:px-8 md:py-5">
       <Header />
       {children}
     </main>
