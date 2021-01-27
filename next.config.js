037a6e54757def2868a36aa0434055d81533b38a
Alex Chong <alexbchong@pm.me>
2021-01-26 21:51:55 -0800

    Update NextJS config basePath/assetPrefix logic to consider dev or production

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const user = process.env.USER;
+const isLocalDev = process.env.NVM_DIR === "/home/" + user + "/.nvm";
+
 module.exports = {
-  assetPrefix: ".",
+  basePath: isLocalDev ? "" : "/alexchong.github.io",
+  assetPrefix: isLocalDev ? "." : "/alexchong.github.io/",
 };
