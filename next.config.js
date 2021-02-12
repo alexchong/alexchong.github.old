1cf4f859f12481153b5d72bc3693306f71abad2b
Alex Chong <alexbchong@pm.me>
2021-02-12 13:34:12 -0800

    Updated next.config.js with assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
+const isLocalDev = process.env.NVM_DIR === "/home/" + user + "/.nvm";
 
 const nextConfig = {
   basePath: "",
+  assetPrefix: isLocalDev ? "." : "/alexchong.github.io/",
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
