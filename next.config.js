c7d82c951744f3e8eb14c3f47f8839a5dda5c471
Alex Chong <alexbchong@pm.me>
2021-02-12 13:21:07 -0800

    Major update on site dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-const user = process.env.USER;
-const isLocalDev = process.env.NVM_DIR === "/home/" + user + "/.nvm";
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = {
-  //basePath: isLocalDev ? "" : "/alexchong.github.io",
-  assetPrefix: isLocalDev ? "." : "/alexchong.github.io/",
+const nextConfig = {
+  basePath: "",
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+    // Handle Markdown files
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+
+    return config;
+  },
 };
+
+module.exports = withPlugins([[optimizedImages]], nextConfig);
