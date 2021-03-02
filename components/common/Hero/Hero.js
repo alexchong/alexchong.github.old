0f0a68462442038dde25a7bc2f4711269518ab9f
Alex Chong <alexbchong@pm.me>
2021-03-02 00:55:07 -0800

    Fix SEO to show name and desc on Index; add resume

--- components/common/Hero/Hero.js
+++ components/common/Hero/Hero.js
@@ -2,17 +2,16 @@ import Link from "next/link";
 import { getSiteMetaData } from "@utils/helpers";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faFilePdf } from "@fortawesome/free-solid-svg-icons";
 // import { Image } from "..";
 
 export function Hero() {
-  const { author, social } = getSiteMetaData();
+  const { author, description, social } = getSiteMetaData();
 
   return (
     <section className="mb-6">
       <p className="text-2xl">
-        Hi there, I'm {author.firstname} - I like to write about web, scripting,
-        and security stuff in general.
+        Hi there, I'm {author.firstName} - {description}
       </p>
       <p className="mb-3 text-pink-600">
         <Link href="/about">Find out more</Link>
@@ -20,14 +19,14 @@ export function Hero() {
       <ul className="flex flex-grow items-center text-2xl">
         <li className="cursor-pointer mr-5">
           <Link href={social.linkedin}>
-            <a>
+            <a target="_blank">
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
           </Link>
         </li>
         <li className="cursor-pointer mr-5">
           <Link href={social.github}>
-            <a>
+            <a target="_blank">
               <FontAwesomeIcon icon={faGithub} />
             </a>
           </Link>
@@ -36,6 +35,13 @@ export function Hero() {
           <Link href="mailto:alex.b.chong@outlook.com">
             <a>
               <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+          </Link>
+        </li>
+        <li className="cursor-pointer mr-5">
+          <Link href="/Alex Chong Resume.pdf">
+            <a target="_blank">
+              <FontAwesomeIcon icon={faFilePdf} />
             </a>
           </Link>
         </li>
